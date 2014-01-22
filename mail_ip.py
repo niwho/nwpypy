9cfa3ffdce1dd798af69bfffe3d871e7aa5b9ebd
niwho <niwho@126.com>
2014-01-22 15:55:47 +0800

    mail_ip.py 监测系统ip(内外网)变化，如有变动则自动发送邮件

--- mail_ip.py
+++ mail_ip.py
@@ -7,14 +7,21 @@ from bs4 import BeautifulSoup
 import re
 import socket
 
+
+import smtplib
+from email.mime.text import MIMEText
+import datetime
+import time
+
 class IP_PARSE_ERROR(Exception):
     def __init__(self):
         Exception.__init__(self)
 
 class WhatIP(object):
     def __init__(self):
-        pass
+        self.preipinfo ='-1'
     
+        self.ipinfo = '0'
     def getInnerIP(self):
         """
         Returns the actual ip of the local machine.
@@ -31,7 +38,8 @@ class WhatIP(object):
             csock.close()
             return addr
         except socket.error:
-            return "127.0.0.1"
+            #return "127.0.0.1"
+            raise IP_PARSE_ERROR 
 
     def getIP(self):
         #raise IP_PARSE_ERROR 
@@ -44,11 +52,63 @@ class WhatIP(object):
               if matchs:
                   return matchs.group(1)
         raise IP_PARSE_ERROR 
+    def getIPInfo(self):
+        try:
+            self.ipinfo = 'LAN:%s\nWAN:%s'%(test.getInnerIP(),test.getIP())
+            print self.ipinfo
+        except Exception:
+            raise IP_PARSE_ERROR 
 
+    def mailIP(self):
+        #ip有变化才发送邮件
+        if self.ipinfo == self.preipinfo:
+            return
+        else:
+            self.preipinfo = self.ipinfo
+        mail_server = 'smtp.126.com'
+        mail_port = 25
+        mail_user =  'alladinfo@126.com'
+        send_to = 'niwho@126.com'
+        try:
+            smtp_client = smtplib.SMTP(mail_server,mail_port)
+            smtp_client.login('alladinfo@126.com','ADINFO')  
+
+            today = datetime.date.today()
+            msg = MIMEText( self.ipinfo )
+            msg[ 'Subject' ] = 'IP For RaspberryPi on %s' % today.strftime('%b %d %Y')
+            msg[ 'From' ] =mail_user
+            msg[ 'To' ] =send_to
+            smtp_client.sendmail( mail_user, [send_to], msg.as_string() )
+            
+        except smtplib.SMTPException:
+            print "send mail error."
+        except Exception, what:
+            print what 
+
+    def listenIP(self):#监控ip变换，自动发送邮件
+        while(1):
+            try:
+                self.getIPInfo()#可能会有异常
+            except IP_PARSE_ERROR,what:
+                print what
+                time.sleep(60)#注意此函数以秒为单位
+                continue
+            except Exception, what:
+                print what 
+                time.sleep(60)#注意此函数以秒为单位
+                continue
+            #无异常则发送邮件
+            self.mailIP()
+            time.sleep(1)
+            
 if __name__ == '__main__':
     try:
         test = WhatIP()
-        print test.getInnerIP()
-        print test.getIP()
+        test.listenIP()
+        #test.getIPInfo()
+        #ipinfo = '%s,%s'%(test.getInnerIP(),test.getIP())
+        #print ipinfo
+        #print test.getInnerIP()
+        #print test.getIP()
     except IP_PARSE_ERROR:
         print "获取系统外网IP失败！"#IP_PARSE_ERROR 
